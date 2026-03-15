f231446d35d7fe8e8c8ae8de3ff988aeffce4e81
Bengalistore <krjnstore@gmail.com>
2026-03-15 14:55:38 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,90 @@
+var database = {
+
+"918957":{
+type:"movie",
+title:"Lawho Gouranger Naam Re",
+link:"https://new5.cinecloud.site/x/6152451d"
+},
+
+"299534":{
+type:"movie",
+title:"Avengers Endgame",
+link:"https://example.com/movie2"
+},
+
+"27205":{
+type:"movie",
+title:"Inception",
+link:"https://example.com/movie3"
+},
+
+"1399":{
+type:"series",
+title:"Game of Thrones",
+episodes:[
+
+{
+name:"Episode 1",
+link:"https://example.com/got1"
+},
+
+{
+name:"Episode 2",
+link:"https://example.com/got2"
+},
+
+{
+name:"Episode 3",
+link:"https://example.com/got3"
+}
+
+]
+},
+
+"82856":{
+type:"series",
+title:"The Mandalorian",
+episodes:[
+
+{
+name:"Episode 1",
+link:"https://example.com/m1"
+},
+
+{
+name:"Episode 2",
+link:"https://example.com/m2"
+},
+
+{
+name:"Episode 3",
+link:"https://example.com/m3"
+}
+
+]
+},
+
+"94997":{
+type:"series",
+title:"House of the Dragon",
+episodes:[
+
+{
+name:"Episode 1",
+link:"https://example.com/h1"
+},
+
+{
+name:"Episode 2",
+link:"https://example.com/h2"
+},
+
+{
+name:"Episode 3",
+link:"https://example.com/h3"
+}
+
+]
+}
+
+};
